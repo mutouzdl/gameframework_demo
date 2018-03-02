14765b359566f32252dae212870366c6c90e21da
钟迪龙 <musicvs@163.com>
2018-03-02 17:18:13 +0800

    提交项目

---
+++ Assets/Demo1/Demo1_ProcedureHelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using GameFramework;
+using GameFramework.Procedure;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+/// <summary>
+/// Demo1-HelloWorld
+/// </summary>
+public class Demo1_ProcedureHelloWorld : ProcedureBase {
+	protected override void OnEnter (ProcedureOwner procedureOwner) {
+		base.OnEnter (procedureOwner);
+
+		string welcomeMessage = "HelloWorld!";
+		Log.Info (welcomeMessage);
+		Log.Warning (welcomeMessage);
+		Log.Error (welcomeMessage);
+	}
+}
---
+++ Assets/GameFramework/Scripts/Editor/Inspector/NetworkComponentInspector.cs
@@ -0,0 +1,63 @@
+﻿//------------------------------------------------------------
+// Game Framework v3.x
+// Copyright © 2013-2017 Jiang Yin. All rights reserved.
+// Homepage: http://gameframework.cn/
+// Feedback: mailto:jiangyin@gameframework.cn
+//------------------------------------------------------------
+
+using GameFramework.Network;
+using UnityEditor;
+using UnityGameFramework.Runtime;
+
+namespace UnityGameFramework.Editor
+{
+    [CustomEditor(typeof(NetworkComponent))]
+    internal sealed class NetworkComponentInspector : GameFrameworkInspector
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (!EditorApplication.isPlaying)
+            {
+                EditorGUILayout.HelpBox("Available during runtime only.", MessageType.Info);
+                return;
+            }
+
+            NetworkComponent t = (NetworkComponent)target;
+
+            if (PrefabUtility.GetPrefabType(t.gameObject) != PrefabType.Prefab)
+            {
+                EditorGUILayout.LabelField("Network Channel Count", t.NetworkChannelCount.ToString());
+
+                INetworkChannel[] networkChannels = t.GetAllNetworkChannels();
+                foreach (INetworkChannel networkChannel in networkChannels)
+                {
+                    DrawNetworkChannel(networkChannel);
+                }
+            }
+
+            Repaint();
+        }
+
+        private void OnEnable()
+        {
+
+        }
+
+        private void DrawNetworkChannel(INetworkChannel networkChannel)
+        {
+            EditorGUILayout.BeginVertical("box");
+            {
+                EditorGUILayout.LabelField(networkChannel.Name, networkChannel.Connected ? "Connected" : "Disconnected");
+                EditorGUILayout.LabelField("Network Type", networkChannel.NetworkType.ToString());
+                EditorGUILayout.LabelField("Local Address", networkChannel.Connected ? string.Format("{0}:{1}", networkChannel.LocalIPAddress.ToString(), networkChannel.LocalPort.ToString()) : "Unknown");
+                EditorGUILayout.LabelField("Remote Address", networkChannel.Connected ? string.Format("{0}:{1}", networkChannel.RemoteIPAddress.ToString(), networkChannel.RemotePort.ToString()) : "Unknown");
+                EditorGUILayout.LabelField("Heart Beat Interval", networkChannel.HeartBeatInterval.ToString());
+            }
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.Separator();
+        }
+    }
+}
