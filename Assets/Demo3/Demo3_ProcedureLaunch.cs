b2d1eed63fef166ebd52016f125abfe095c83eba
钟迪龙 <musicvs@163.com>
2018-03-07 17:09:50 +0800

    新增Demo3

---
+++ Assets/Demo3/Demo3_ProcedureLaunch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameFramework;
+using GameFramework.Procedure;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+public class Demo3_ProcedureLaunch : ProcedureBase {
+	protected override void OnEnter(ProcedureOwner procedureOwner)
+	{
+		base.OnEnter(procedureOwner);
+
+		SceneComponent scene
+			= UnityGameFramework.Runtime.GameEntry.GetComponent<SceneComponent>();
+
+		// 切换场景
+		scene.LoadScene("Demo3_Menu", this);
+
+		// 切换流程
+        ChangeState<Demo3_ProcedureMenu>(procedureOwner);
+	}
+}
+
---
+++ Assets/Demo3/Demo3_UIMenu.cs
@@ -0,0 +1,7 @@
+﻿using GameFramework;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+public class Demo3_UIMenu : UIFormLogic {
+	
+}
