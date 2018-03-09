68d2fccef5e6871985b573663c2bc982f3da4020
钟迪龙 <musicvs@163.com>
2018-03-09 17:08:05 +0800

    增加Demo4，未测试

---
+++ Assets/Demo4/Demo4_ProcedureLaunch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameFramework;
+using GameFramework.Procedure;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+public class Demo4_ProcedureLaunch : ProcedureBase {
+	protected override void OnEnter(ProcedureOwner procedureOwner)
+	{
+		base.OnEnter(procedureOwner);
+
+		SceneComponent scene
+			= UnityGameFramework.Runtime.GameEntry.GetComponent<SceneComponent>();
+
+		// 切换场景
+		scene.LoadScene("Demo4_Menu", this);
+
+		// 切换流程
+        ChangeState<Demo4_ProcedureMenu>(procedureOwner);
+	}
+}
+
---
+++ Assets/Demo4/Demo4_ProcedureMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameFramework;
+using GameFramework.Event;
+using GameFramework.Procedure;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+public class Demo4_ProcedureMenu : ProcedureBase {
+	protected override void OnEnter(ProcedureOwner procedureOwner)
+	{
+		base.OnEnter(procedureOwner);
+
+		// 加载框架Event组件
+		EventComponent Event
+			= UnityGameFramework.Runtime.GameEntry.GetComponent<EventComponent>();
+
+		// 加载框架UI组件
+		UIComponent UI
+			= UnityGameFramework.Runtime.GameEntry.GetComponent<UIComponent>();
+
+		// 订阅UI加载成功事件
+		Event.Subscribe(OpenUIFormSuccessEventArgs.EventId, OnOpenUIFormSuccess);
+
+		// 加载UI
+		UI.OpenUIForm("Assets/Demo4/UI_Menu.prefab", "DefaultGroup", this);
+	}
+	
+	private void OnOpenUIFormSuccess(object sender, GameEventArgs e)
+	{
+		OpenUIFormSuccessEventArgs ne = (OpenUIFormSuccessEventArgs)e;
+
+		// 判断userData是否为自己
+		if (ne.UserData != this)
+		{
+			return;
+		}
+
+		Log.Debug("UI_Menu：恭喜你，成功地召唤了我。");
+	}
+}
