1e6795d2765b7b1f761b36a6a2e4927368cd9b77
钟迪龙 <musicvs@163.com>
2018-03-15 07:58:57 +0800

    增加Demo5和对应教程PDF

---
+++ Assets/Demo5/DRHero.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameFramework.DataTable;
+using UnityEngine;
+
+public class DRHero : IDataRow
+{
+    public int Id { get; protected set; }
+    public string Name { get; private set; }
+    public int Atk { get; private set; }
+
+    public void ParseDataRow(string dataRowText)
+    {
+        string[] text = dataRowText.Split('\t');
+		int index = 0;
+        index++;    // 跳过#注释列
+        
+		Id = int.Parse(text[index++]);
+		Name = text[index++];
+		Atk = int.Parse(text[index++]);
+    }
+}
---
+++ Assets/Demo5/Demo5_ProcedureLaunch.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameFramework;
+using GameFramework.DataTable;
+using GameFramework.Event;
+using GameFramework.Procedure;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+public class Demo5_ProcedureLaunch : ProcedureBase {
+	protected override void OnEnter(ProcedureOwner procedureOwner)
+	{
+		base.OnEnter(procedureOwner);
+
+		// 获取框架事件组件
+		EventComponent Event
+			= UnityGameFramework.Runtime.GameEntry.GetComponent<EventComponent>();
+
+		// 获取框架数据表组件
+		DataTableComponent DataTable
+			= UnityGameFramework.Runtime.GameEntry.GetComponent<DataTableComponent>();
+
+		// 订阅加载成功事件
+		Event.Subscribe(UnityGameFramework.Runtime.LoadDataTableSuccessEventArgs.EventId, OnLoadDataTableSuccess);
+
+		// 加载配置表
+		DataTable.LoadDataTable<DRHero>("Hero", "Assets/Demo5/Hero.txt");
+	}
+
+	private void OnLoadDataTableSuccess(object sender, GameEventArgs e)
+	{
+        UnityGameFramework.Runtime.LoadDataTableSuccessEventArgs eventArgs 
+			= e as UnityGameFramework.Runtime.LoadDataTableSuccessEventArgs;
+
+        Log.Debug("加载成功:" + eventArgs.DataRowType);
+
+		// 获取框架数据表组件
+		DataTableComponent DataTable
+			= UnityGameFramework.Runtime.GameEntry.GetComponent<DataTableComponent>();
+
+		// 获得数据表
+		IDataTable<DRHero> dtScene = DataTable.GetDataTable<DRHero>();
+		
+		// 获得所有行
+		DRHero[] drHeros = dtScene.GetAllDataRows();
+
+		Log.Debug("drHeros:" + drHeros.Length);
+
+		// 根据行号获得某一行
+		DRHero drScene = dtScene.GetDataRow(1); // 或直接使用 dtScene[1]
+		if (drScene != null)
+		{
+			// 此行存在，可以获取内容了
+			string name = drScene.Name;
+			int atk = drScene.Atk;
+
+        	Log.Debug("name:" + name + ", atk:" + atk);
+		}
+		else
+		{
+			// 此行不存在
+		}
+
+		// 获得满足条件的所有行
+		DRHero[] drScenesWithCondition = dtScene.GetAllDataRows(x => x.Id > 0);
+		
+		// 获得满足条件的第一行
+		DRHero drSceneWithCondition = dtScene.GetDataRow(x => x.Name == "mutou");
+	
+	}
+}
+
